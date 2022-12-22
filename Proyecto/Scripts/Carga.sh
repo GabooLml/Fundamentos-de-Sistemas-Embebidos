765f15efb50ea48669e74f08d0aeb716b97a65ff
GabooLml <gabrielrojasmendez@gmail.com>
2022-12-21 21:00:42 -0600

    Códigos comentados

--- Proyecto/Scripts/Carga.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-python /home/Gaboo/Proyecto/Temperatura.py
-
--- Proyecto/Scripts/Encendido.sh
+++ Proyecto/Scripts/Encendido.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
 
+#Token del bot de Telegram
 TOKEN="5750664400:AAGFA9MkVc9INtQlifD270DsjrpWul1-gQk"
+
+#ID del chat al que se mandarán las alertas
 ID="5755555473"
+
+#Mensaje que se va a mandar al chat
 MENSAJE="HERMES se ha puesto en línea"
+
+#URL de la API por la cual se harán llegar las alertas
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
+#Comando que concatena las variables en una URL para realizar la petición
 curl -s -X POST $URL -d chat_id=$ID -d text="$MENSAJE"
 
